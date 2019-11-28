5df375cb2f8eec97142e675948912c40b5699747
LucasDambroz <55919951+LucasDambroz@users.noreply.github.com>
2019-11-27 21:35:04 -0300

    27-11

---
+++ assets/js/recuperarSenha.js
@@ -0,0 +1,15 @@
+function ocultar(){
+ {
+        switch (obj.id) {
+        case 'default':
+         
+        document.getElementById('painel').style.display="block";
+         
+        break
+        case 'id_form_contato_div':
+         
+        document.getElementById('painel').style.display="none";
+         
+        break
+         
+}}};
